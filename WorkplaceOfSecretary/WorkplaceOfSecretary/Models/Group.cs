2526172cd2e3b078782bd0190e77e0325823c561
Maxim <sipakovmaksim@gmail.com>
2019-05-20 16:38:49 +0300

    Add Group model

---
+++ WorkplaceOfSecretary/WorkplaceOfSecretary/Models/Group.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WorkplaceOfSecretary.Models
+{
+    [Table("Group")]
+    public class Group
+    {
+        public int ID { get; set; }
+        [Column("IdSpecialty")]
+        public int SpecialtyID { get; set; }
+        public string NumberOfGroup { get; set; }
+        public string ComissionMembers { get; set; }
+
+        public virtual Specialty Specialty { get; set; }
+        public virtual ICollection<Student> Students { get; set; }
+    }
+}
