4e5d32d275ba70380e4bb8516971a03ce63b6a66
Maxim <sipakovmaksim@gmail.com>
2019-05-21 00:21:08 +0300

    Implement CRUD for SEB

---
+++ WorkplaceOfSecretary/WorkplaceOfSecretary/Controllers/SEBController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WorkplaceOfSecretary.DAL;
+using WorkplaceOfSecretary.Models;
+
+namespace WorkplaceOfSecretary.Controllers
+{
+    public class SEBController : Controller
+    {
+        private WoSContext db = new WoSContext();
+
+        // GET: SEB
+        public ActionResult Index()
+        {
+            return View(db.SEBs.ToList());
+        }
+
+        // GET: SEB/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SEB sEB = db.SEBs.Find(id);
+            if (sEB == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sEB);
+        }
+
+        // GET: SEB/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SEB/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(SEB sEB)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.SEBs.Add(sEB);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (DbUpdateException /* ex */)
+            {
+                //Log the error (uncomment ex variable name and write a log.
+                ModelState.AddModelError("", "Unable to save changes. " +
+                    "Try again, and if the problem persists " +
+                    "see your system administrator.");
+            }
+            return View(sEB);
+        }
+
+        // GET: SEB/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SEB sEB = db.SEBs.Find(id);
+            if (sEB == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sEB);
+        }
+
+        // POST: SEB/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,NameOfSEB")] SEB sEB)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    db.Entry(sEB).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch (DbUpdateException /* ex */)
+                {
+                    //Log the error (uncomment ex variable name and write a log.)
+                    ModelState.AddModelError("", "Unable to save changes. " +
+                        "Try again, and if the problem persists, " +
+                        "see your system administrator.");
+                }
+            }
+            return View(sEB);
+        }
+
+        // GET: SEB/Delete/5
+        public ActionResult Delete(int? id, bool? saveChangesError = false)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewBag.ErrorMessage = "Delete failed. Try again, and if the problem persists see your system administrator.";
+            }
+            SEB sEB = db.SEBs.Find(id);
+            if (sEB == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sEB);
+        }
+
+        // POST: SEB/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            try
+            {
+                SEB sEB = db.SEBs.Find(id);
+                db.SEBs.Remove(sEB);
+                db.SaveChanges();
+            }
+            catch (DataException/* dex */)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                return RedirectToAction("Delete", new { id = id, saveChangesError = true });
+            }
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
