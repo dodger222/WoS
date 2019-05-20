19e1f222a5a1d9fd6580918718f74fabb7eb9a1c
Maxim <sipakovmaksim@gmail.com>
2019-05-21 00:13:34 +0300

    Add SEB model

---
+++ WorkplaceOfSecretary/WorkplaceOfSecretary/Models/SEB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WorkplaceOfSecretary.Models
+{
+    [Table("SEB")]
+    public class SEB
+    {
+        public int ID { get; set; }
+        public string NameOfSEB { get; set; }
+
+        //public virtual ICollection<Meeting> Meetings { get; set; }
+        //public virtual ICollection<Committee> Committees { get; set; }
+    }
+}
