984c83af208c7acd3881e7faa74ea796bc0cb5e9
Maxim <sipakovmaksim@gmail.com>
2019-05-20 16:40:17 +0300

    Add Student model

---
+++ WorkplaceOfSecretary/WorkplaceOfSecretary/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WorkplaceOfSecretary.Models
+{
+    [Table("Student")]
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Patronymic { get; set; }
+        [Column("IdGroup")]
+        public int GroupID { get; set; }
+        public decimal AverageScore { get; set; }
+        public bool Foreigner { get; set; }
+
+        public Group Group { get; set; }
+        //public ICollection<Protocol> Protocols { get; set; }
+    }
+}
