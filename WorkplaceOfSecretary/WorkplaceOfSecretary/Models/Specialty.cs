75c6ea12e4fcdc5e49afa4fc4b179e8e5f81746c
Maxim <sipakovmaksim@gmail.com>
2019-05-20 16:28:08 +0300

    Add Specialty model

---
+++ WorkplaceOfSecretary/WorkplaceOfSecretary/Models/Specialty.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WorkplaceOfSecretary.Models
+{
+    [Table("Specialty")]
+    public class Specialty
+    {
+        public int ID { get; set; }
+        public string NameOfSpecialty { get; set; }
+        public string NumberOfSpecialty { get; set; }
+
+        public virtual ICollection<Group> Groups { get; set; }
+    }
+}
