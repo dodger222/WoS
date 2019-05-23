3de77406648e7065a615ed7a02472832b1b503b5
Maxim <sipakovmaksim@gmail.com>
2019-05-23 14:30:58 +0300

    Add GET method Create and Create view for DataFroList

---
+++ WorkplaceOfSecretary/WorkplaceOfSecretary/Controllers/DataForListController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WorkplaceOfSecretary.DAL;
+using WorkplaceOfSecretary.Models;
+
+namespace WorkplaceOfSecretary.Controllers
+{
+    public class DataForListController : Controller
+    {
+        private WoSContext db = new WoSContext();
+
+        public ActionResult Create()
+        {
+            List<SEB> sebs = new List<SEB>();
+            sebs.Add(null);
+            sebs.AddRange(db.SEBs.ToList());
+
+            ViewBag.Sebs = new SelectList(sebs, "NameOfSEB", "NameOfSEB");
+
+            List<Group> groups = new List<Group>();
+            groups.Add(null);
+            groups.AddRange(db.Groups.ToList());
+
+            ViewBag.GroupID = new SelectList(groups, "ID", "NumberOfGroup");
+
+            return View();
+        }
+    }
+}
