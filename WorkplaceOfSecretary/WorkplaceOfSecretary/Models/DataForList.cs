4a75d7d5c906daebe2e3753b2a51d63e8c38edb3
Maxim <sipakovmaksim@gmail.com>
2019-05-23 13:52:00 +0300

    Add DataForList model

---
+++ WorkplaceOfSecretary/WorkplaceOfSecretary/Models/DataForList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WorkplaceOfSecretary.Models
+{
+    public class DataForList
+    {
+        public string SEB { get; set; }
+        public DateTime Date { get; set; }
+        public string Group { get; set; }
+
+        public List<Student> Students { get; set; }
+    }
+}
