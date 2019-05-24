1e5317383b8e6a4b1e61d01c277ade7844ea52ef
Maxim <sipakovmaksim@gmail.com>
2019-05-24 13:23:13 +0300

    Fix bug with average score of student

--- WorkplaceOfSecretary/WorkplaceOfSecretary/Models/Student.cs
+++ WorkplaceOfSecretary/WorkplaceOfSecretary/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
@@ -15,6 +16,9 @@ namespace WorkplaceOfSecretary.Models
         public string Patronymic { get; set; }
         [Column("IdGroup")]
         public int GroupID { get; set; }
+
+        [Required]
+        [Range(typeof(decimal), "4,0", "10,0", ErrorMessage = "Наименьшая оценка - 4, максимальная - 10, в качестве разделителя дробной и целой части используется запятая")]
         public decimal AverageScore { get; set; }
         public bool Foreigner { get; set; }
 
